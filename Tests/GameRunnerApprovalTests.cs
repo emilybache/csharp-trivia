ed627a1331bcfef4c7ae1316ba4dff9593b50ac0
Ivett Ördög <devillmeister@gmail.com>
2025-03-28 11:24:26 +0100

    Initial commit

---
+++ Tests/GameRunnerApprovalTests.cs
@@ -0,0 +1,22 @@
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using Xunit;
+using System.IO;
+
+public class GameRunnerApprovalTests
+{
+    [Fact]
+    [UseReporter(typeof(WinMergeReporter))]
+    public void GameRunner_Output_Should_Be_Approved()
+    {
+        // Arrange
+        var output = new StringWriter();
+        Console.SetOut(output);
+
+        // Act
+        GameRunner.Main(new string[] { });
+
+        // Assert
+        Approvals.Verify(output.ToString());
+    }
+}
